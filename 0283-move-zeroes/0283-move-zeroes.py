03bfedb07c9c55e4b1d26aff93779c7fe0001a95
Anil Yadav <anil.kanhaiyalal2023@sait.ac.in>
2026-07-07 17:46:24 +0530

    Time: 3 ms (90.03%), Space: 13.7 MB (19.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,14 +1,13 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        with_zero = []
-        without_zero = []
+        zero=[]
+        nonzero=[]
         for i in nums:
-            if i!=0:
-                without_zero.append(i)
+            if i==0:
+                zero.append(i)
             else:
-                with_zero.append(i)
-        
-        new = without_zero + with_zero
+                nonzero.append(i)
+        new=nonzero+zero
         for i in range(len(nums)):
             nums[i] = new[i]
         
