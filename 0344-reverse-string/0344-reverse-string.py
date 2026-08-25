ab0c5d9dc5f2a3edd11a7c351c44b4270346bfb0
Anil Yadav <anil.kanhaiyalal2023@sait.ac.in>
2026-08-26 00:41:07 +0530

    Time: 0 ms (100%), Space: 19.9 MB (43.48%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def reverseString(self, s):
-        left = 0
-        right = len(s)-1
-        for i in range(len(s)):
-            if left<right:
-                s[left],s[right] = s[right],s[left]
-                left +=1
-                right -=1
-            else:
-                break
-        
+        # left = 0
+        # right = len(s)-1
+        # for i in range(len(s)):
+        #     if left<right:
+        #         s[left],s[right] = s[right],s[left]
+        #         left +=1
+        #         right -=1
+        #     else:
+        #         break
+        s=s.reverse()
